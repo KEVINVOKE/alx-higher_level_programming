9f098b9d2eb35a09e3b8ab05471e4f4c23900874
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-06-02 22:21:55 +0300

    Update 100-my_calculator.py
    My commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-# 100-my_calculator.py
-
-
-if __name__ == "__main__":
-    """ basic maths operations."""
+if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
-    import sys
-
-    if len(sys.argv) - 1 != 3:
+    from sys import argv
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
+    else:
+        ope = argv[2]
+        a = int(argv[1])
+        b = int(argv[3])
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("unknown operation. available operations: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+        if ope == '+':
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+        elif ope == '-':
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+        elif ope == "*":
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+        elif ope == '/':
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
