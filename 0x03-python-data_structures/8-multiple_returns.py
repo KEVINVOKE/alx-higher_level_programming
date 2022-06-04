a6d94f3935b760dc737e0e5da065856a578236ba
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-06-04 23:54:34 +0300

    Update 8-multiple_returns.py
    My commit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,10 @@
 
 
 def multiple_returns(sentence):
-    """Function that returns a tuple with length of a string and its first character."""
+    """
+    A function that returns a tuple with 
+    the length of a string and its first character.
+    """
     if sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
