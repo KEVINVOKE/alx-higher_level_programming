29f25e475b784580a53a187221cd1cbf1f29f646
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-06-05 14:47:49 +0300

    Update 2-replace_in_list.py
    My commit

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list is None:
-        return
-    for i in reversed(my_list):
-        print('{:d}'.format(i))
+
+
+def replace_in_list(my_list, idx, element):
+    n = len(my_list)
+    if idx < 0:
+        return my_list
+    elif idx > n - 1:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
