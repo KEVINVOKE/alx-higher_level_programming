fa1d4d74d879fb8d9d1442129b65290403a7396f
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-06-20 18:45:06 +0300

    Update 2-safe_print_list_integers.py
    My commit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+
+
 def safe_print_list_integers(my_list=[], x=0):
-    output = 0
-    for i in range(x):
+    """
+    a function that prints the first x elements of
+    a list and only integers.
+    """
+    count = 0
+    for element in range(0, x):
         try:
-            print('{:d}'.format(my_list[i]), end='')
-            output += 1
-        except (TypeError, ValueError):
-            pass
-    print()
-    return output
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
+            print("{:d}".format(my_list[element]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return count
